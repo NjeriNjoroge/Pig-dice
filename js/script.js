a366d37944f213b2eb7f962957ed6d2019cc1aae
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-06-16 14:07:16 +0300

    managed to make the players random numbers add

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@ var playerOne = {
   score: 0,
   name: "tom",
 };
+var totalScre =0;
+
+
 
 $(document).ready(function(){
   $("button#submit").click(function(){
-  playerOne.score = Math.floor((Math.random() * 6) + 1);
-  $("div.output").append(playerOne.score);
+    //when clicked it generated random number
+  var score1 = Math.floor((Math.random() * 6) + 1);
+  playerOne.score = score1
+
+  if (playerOne.score <= 1){
+    console.log("oops!")
+  } else {
+    console.log("keep playing")
+  totalScre+=score1;
+  }
+  $("div.output").text(totalScre);
   });
 });
