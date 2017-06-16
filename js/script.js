c91eb8f88dfa2a833003e1db2655ccbb53e03ceb
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-06-16 17:03:40 +0300

    the hold button works and it displays the users total score when clicked

--- js/script.js
+++ js/script.js
@@ -2,28 +2,35 @@ var playerOne = {
   score: 0,
   name: "tom",
 };
+
 var totalScre = 0;
 var scoreArray = [];
 
 
+
+//user logic
 $(document).ready(function(){
   $("button#submit").click(function(){
     //when clicked it generated random number
   var score1 = Math.floor((Math.random() * 6) + 1);
   playerOne.score = score1
 
-  if (playerOne.score <= 1){
-    console.log("oops!")
+  if (playerOne.score === 1){
+    alert("Uko na one")
+    $('#submit').prop('disabled', true)
   } else {
     console.log("keep playing")
   totalScre+=score1;
   }
-  $("div.output").text("score" + " " + totalScre);
+  $("div.output").append("score" + " " + totalScre);
   });
 //function for when the player holds
 $("button#hold1").click(function(){
-  scoreArray.push(totalScre);
-$("div.output1").append("hold score is" + " " + scoreArray);
+
+  var total = scoreArray.push(totalScre);
+  for(var i in scoreArray) { total = scoreArray[i]; }
+  //scoreArray.reduce((a, b) => a + b, 0);
+$("div.output1").append("hold score is" + " " + total);
 })
 
 });
